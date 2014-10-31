d080eda9c1b7e60f801edcf2809a48d787b6a82b
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2014-10-31 14:59:25 +0000

    footer position adjust script

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,22 @@ require([
   'bootstrap/transition',
   'bootstrap/collapse',
   ], function ($, bt, bc) {
+    var resizeViewPort = function () {
+      /* resizes the main element,
+         in order to always push footer to the bottom */
+
+      $('main').css({'min-height': function () {
+        return  ($(window).height() -
+                  ($('nav.main-navigation').outerHeight(true) +
+                    $('footer.main-footer').outerHeight(true))) + 'px';
+      }});
+    };
 
     $( document ).ready(function() {
 
-
+      $(window).on('resize', resizeViewPort).trigger('resize');
       //debug
       console.log('hello');
-
       // $('#myModal').foundation('reveal', 'open');
     });
 });
