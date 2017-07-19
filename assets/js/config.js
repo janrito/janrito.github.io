7d1479eb25f8258991d5838dd465c9309c3cf2f1
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2017-07-19 01:40:02 +0100

    Removed unnecessary assets

--- assets/js/config.js
+++ assets/js/config.js
@@ -11,40 +11,12 @@ requirejs.config({
   },
 
   baseUrl: "/assets",
-  // urlArgs: "bust=" + (new Date()).getTime(),
+  urlArgs: "bust=" + (new Date()).getTime(),
   paths: {
     "jquery": "vendor/jquery/dist/jquery.min",
-
-    /* Bootstrap */
-    'bootstrap': 'vendor/bootstrap-sass-official/assets/javascripts/bootstrap',
 
     // Google Analytics
     'EventEmitter': 'vendor/event-emitter/dist/EventEmitter',
     'GA': 'vendor/requirejs-google-analytics/dist/GoogleAnalytics',
-
-    // lunr.js
-    'lunr': 'vendor/lunr.js/lunr.min',
-    'lunr.stemmer.support': 'vendor/lunr-languages/min/lunr.stemmer.support.min',
-    'lunr.es': 'vendor/lunr-languages/min/lunr.es.min',
-
-    // localforage
-    "localforage": 'vendor/localforage/dist/localforage.min',
-
-    'mustache': 'vendor/mustache/mustache',
-
-  },
-  shim: {
-    'bootstrap/affix':      { deps: ['jquery'], exports: '$.fn.affix' },
-    'bootstrap/alert':      { deps: ['jquery'], exports: '$.fn.alert' },
-    'bootstrap/button':     { deps: ['jquery'], exports: '$.fn.button' },
-    'bootstrap/carousel':   { deps: ['jquery'], exports: '$.fn.carousel' },
-    'bootstrap/collapse':   { deps: ['jquery'], exports: '$.fn.collapse' },
-    'bootstrap/dropdown':   { deps: ['jquery'], exports: '$.fn.dropdown' },
-    'bootstrap/modal':      { deps: ['jquery'], exports: '$.fn.modal' },
-    'bootstrap/popover':    { deps: ['jquery'], exports: '$.fn.popover' },
-    'bootstrap/scrollspy':  { deps: ['jquery'], exports: '$.fn.scrollspy' },
-    'bootstrap/tab':        { deps: ['jquery'], exports: '$.fn.tab' },
-    'bootstrap/tooltip':    { deps: ['jquery'], exports: '$.fn.tooltip' },
-    'bootstrap/transition': { deps: ['jquery'], exports: '$.fn.transition' }
   },
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -15,51 +15,9 @@ require(['config'], function (config) {
   // Main
   require([
     'jquery',
-    'bootstrap/transition',
-    'bootstrap/collapse',
-    'bootstrap/tooltip',
-    ], function ($, bt, bc, btol) {
-
-      var resizePositioning = function () {
-        resizeViewPort();
-        imagesBaselineFix();
-      };
-
-      var imagesBaselineFix = function () {
-        // adjusts the lower margin for all images so that hte following text
-        // matches the baseline on the rest of the document
-
-        var lineHeight = $('main p').first().css('line-height') || '0',
-            lineHeightComputed = parseInt(lineHeight.replace('px', ''));
-
-        $('main img').each(function() {
-              var $this = $(this),
-                  height = $this.outerHeight(false),
-                  extra =((height) % (lineHeightComputed)),
-                  newMargin = lineHeightComputed - extra;
-
-            $this.css('margin-bottom', newMargin);
-        });
-      };
-
-      var resizeViewPort = function () {
-         // resizes the main element,
-         // in order to always push footer to the bottom
-
-        $('main').css({'min-height': function () {
-          return  ($(window).height() -
-                    ($('nav.main-navigation').outerHeight(true) +
-                      $('footer.main-footer').outerHeight(true))) + 'px';
-        }});
-      };
-
+    ], function ($,) {
       $( document ).ready(function() {
-
-        $(window).on('resize', resizePositioning).trigger('resize');
-        $('[data-toggle="tooltip"]').tooltip();
-
         console.log('hello');
-
       });
   });
 
